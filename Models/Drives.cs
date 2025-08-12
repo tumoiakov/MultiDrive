2e2ad6582879d36379208dadb30aae0323d26e4e
tumoiakov <tumoyakov.work@outlook.com>
2025-08-12 16:38:28 +0700

    feat(MainPage): Add viewmodel and basic use case of FileWatcher

---
+++ Models/Drives.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiDrive.Models
+{
+    internal class Drives
+    {
+        DriveInfo[] drivesInfo;
+        public DriveInfo[] DrivesInfo
+        {
+            get => drivesInfo;
+        }
+
+        public Drives()
+        {
+            drivesInfo = new DriveInfo[0];
+            UpdateDrives();
+        }
+
+        public void UpdateDrives()
+        {
+            drivesInfo = DriveInfo.GetDrives();
+            Console.WriteLine(drivesInfo.Length.ToString() + " " + drivesInfo.ToString());
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,94 @@
+﻿using MultiDrive.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiDrive.ViewModels
+{
+    internal class MainViewModel: INotifyPropertyChanged
+    {
+        public string Text => "Sample text from ViewModel";
+        string info = "";
+        public string Info {  
+            get => info; 
+            set
+            {
+                if (info != value)
+                {
+                    info = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        FileSystemWatcher fileSystemWatcher;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public MainViewModel()
+        {
+            Debug.WriteLine("MainViewModel loaded");
+            Drives drives = new Drives();
+            foreach(DriveInfo driveInfo in drives.DrivesInfo)
+            {
+                Info += driveInfo.Name + "; ";
+            }
+            try
+            {
+                fileSystemWatcher = new FileSystemWatcher(); //drives.DrivesInfo[1].RootDirectory.FullName
+                fileSystemWatcher.Path = "C:\\temp";
+                fileSystemWatcher.NotifyFilter = NotifyFilters.Attributes
+                                 | NotifyFilters.CreationTime
+                                 | NotifyFilters.DirectoryName
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.Security
+                                 | NotifyFilters.Size;
+                fileSystemWatcher.Created += OnChanged;
+                fileSystemWatcher.Deleted += OnChanged;
+                fileSystemWatcher.Changed += OnChanged;
+                fileSystemWatcher.Renamed += OnChanged;
+                fileSystemWatcher.Error += OnError;
+
+                fileSystemWatcher.EnableRaisingEvents = true;
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            Info = $"Файл {e.FullPath} был {e.ChangeType}";
+            Debug.WriteLine($"Файл {e.FullPath} был {e.ChangeType}");
+        }
+
+        private static void OnError(object sender, ErrorEventArgs e) =>
+            PrintException(e.GetException());
+
+        private static void PrintException(Exception? ex)
+        {
+            if (ex != null)
+            {
+                Debug.WriteLine($"Message: {ex.Message}");
+                Debug.WriteLine("Stacktrace:");
+                Debug.WriteLine(ex.StackTrace);
+                PrintException(ex.InnerException);
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
